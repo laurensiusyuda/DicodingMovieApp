0e74e552478f68dccf3e37dbaa8f0eff2fc5eb9f
laurensiusyuda <yudalaurensius87@gmail.com>
2022-12-02 12:24:12 +0700

    update test

---
+++ test/helpers/test_helper.dart
@@ -0,0 +1,18 @@
+import 'package:core/data/datasources/db/database_helper.dart';
+import 'package:core/data/datasources/local_data_source.dart';
+import 'package:core/data/datasources/remote_data_source.dart';
+import 'package:core/domain/repositories/repository.dart';
+import 'package:core/utils/network_info.dart';
+import 'package:mockito/annotations.dart';
+import 'package:http/http.dart' as http;
+
+@GenerateMocks([
+  MovieRepository,
+  MovieRemoteDataSource,
+  MovieLocalDataSource,
+  NetworkInfo,
+  DatabaseHelper,
+], customMocks: [
+  MockSpec<http.Client>(as: #MockHttpClient)
+])
+void main() {}
